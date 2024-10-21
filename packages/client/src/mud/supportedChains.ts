6822a8c587ebfe442d96f480f67d035b228bc33c
choguun <visaruth.s@gmail.com>
2024-10-21 10:22:07 +0700

    add base chain in supportedchain

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -69,6 +69,109 @@ export const opBNBTestnet: any = {
   bridgeUrl: "https://opbnb-testnet-bridge.bnbchain.org/deposit",
 };
 
+export const avaxTestnet: any = {
+  id: 43113, // Your custom chain ID
+  name: "Avalanche Fuji Testnet",
+  nativeCurrency: {
+    name: "AVAX",
+    symbol: "AVAX",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://avalanche-fuji-c-chain-rpc.publicnode.com"],
+      webSocket: ["wss://avalanche-fuji-c-chain-rpc.publicnode.com"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "AvaxFujiScan",
+      url: "https://testnet.snowtrace.io/",
+    },
+  },
+  testnet: true,
+  indexerUrl: "http://18.139.116.171:3006",
+  bridgeUrl: "https://core.app/en/tools/testnet-faucet",
+};
+
+export const zetaTestnet: any = {
+  id: 7001, // Your custom chain ID
+  name: "ZETA Testnet",
+  nativeCurrency: {
+    name: "ZETA",
+    symbol: "ZETA",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://zeta-chain-testnet.drpc.org"],
+      webSocket: [
+        "wss://zetachain-athens.blockpi.network/rpc/v1/public/websocket",
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "ZetatestnetScan",
+      url: "https://athens.explorer.zetachain.com",
+    },
+  },
+  testnet: true,
+  bridgeUrl: "",
+};
+
+export const BNBTestnet: any = {
+  id: 97, // Your custom chain ID
+  name: "BNB Testnet",
+  nativeCurrency: {
+    name: "BNB",
+    symbol: "BNB",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: [
+        "https://quick-greatest-sailboat.bsc-testnet.quiknode.pro/84e5105026493e0e7f39982c4c8763b25eb16541",
+      ],
+      webSocket: [
+        "wss://quick-greatest-sailboat.bsc-testnet.quiknode.pro/84e5105026493e0e7f39982c4c8763b25eb16541",
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "BNBtestnetScan",
+      url: "https://testnet.bscscan.com",
+    },
+  },
+  testnet: true,
+  bridgeUrl: "",
+};
+
+export const baseTestnet: any = {
+  id: 84532, // Your custom chain ID
+  name: "Base Sepolia",
+  nativeCurrency: {
+    name: "ETH",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://sepolia.base.org"],
+      webSocket: ["wss://base-sepolia-rpc.publicnode.com"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Basescan",
+      url: "https://sepolia.basescan.org/",
+    },
+  },
+  testnet: true,
+  sourceId: 1,
+};
+
 /*
  * See https://mud.dev/guides/hello-world/add-chain-client
  * for instructions on how to add networks.
@@ -78,4 +181,8 @@ export const supportedChains: MUDChain[] = [
   redstone,
   garnet,
   opBNBTestnet,
+  avaxTestnet,
+  zetaTestnet,
+  BNBTestnet,
+  baseTestnet,
 ];
